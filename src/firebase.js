90c77369554da899bae4231c2dbe9e9443f27afb
Charlotte Rosario <charlottearosario@gmail.com>
2025-07-29 11:14:59 -0400

    fixed env

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "somethings-buddy.firebaseapp.com",
-  databaseURL: "https://somethings-buddy-default-rtdb.firebaseio.com",
-  projectId: "somethings-buddy",
-  storageBucket: "somethings-buddy.firebasestorage.app",
-  messagingSenderId: "1025022953185",
-  appId: "1:1025022953185:web:d16ff1e2fd0c344a77d9a0",
-  measurementId: "G-HL4BPJ7SEL"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,   
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
